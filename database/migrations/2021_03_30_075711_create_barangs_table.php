a42a8429f94ae9e56205c3eb7b8fd3c99c5d818e
aufulkirom-id <aufulkirom-id>
2021-03-30 15:19:30 +0700

    create migration

---
+++ database/migrations/2021_03_30_075711_create_barangs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBarangsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('barangs', function (Blueprint $table) {
+            $table->id('id_barang');
+            $table->string('kode_barang');
+            $table->string('nama_barang');
+            $table->string('kategori_barang');
+            $table->integer('harga',20);
+            $table->integer('qty',10);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('barangs');
+    }
+}
