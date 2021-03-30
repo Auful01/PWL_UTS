1340314be28edb2025829ff508b629167a938fcb
aufulkirom-id <aufulkirom-id>
2021-03-30 23:22:58 +0700

    edit models barang

--- app/Models/Barang.php
+++ app/Models/Barang.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Barang extends Model
 {
     use HasFactory;
+   protected $table = "barangs";
+    public $timestamps = false;
+    protected $primaryKey = 'id_barang';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_barang',
+        'kode_barang',
+        'nama_barang',
+        'kategori_barang',
+        'harga',
+        'qty',
+    ];
 }
